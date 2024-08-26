650cfd33e20ab567038b71b92c0a4b6fd010e7c3
prasad <prasad@annadata.net>
2024-08-26 16:18:54 +0000

    Updates

---
+++ static/js/566.28fe90bc.chunk.js
@@ -0,0 +1,47 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[566],{6566:(e,i,t)=>{t.r(i),t.d(i,{default:()=>u});var n=t(4647),r=t(1319),s=t(4019),d=t(9281),c=t(5639),o=t(2646),l=t(9);const a=(0,l.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,p=(0,l.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,h=(0,l.Ay)(r.A)`
+  flex-direction: ${e=>{let{direction:i}=e;return"left"===i?"row":"row-reverse"}};
+`,x=(0,l.Ay)("div")`
+  position: center;
+  /* max-width: 1080px; */
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,m=(0,l.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,f=(0,l.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #ffff;
+  font-family: "Motiva Sans Light", sans-serif;
+`,y=(0,l.Ay)("p")`
+  font-size: 13px;
+`,j=(0,l.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`;var w=t(579);const u=(0,d.C)()((e=>{let{icon:i,title:t,content:d,section:l,button:u,t:g,id:A,direction:b}=e;return(0,w.jsx)(a,{children:(0,w.jsx)(s.zW,{direction:b,triggerOnce:!0,children:(0,w.jsx)(h,{justify:"space-between",align:"middle",id:A,direction:b,children:(0,w.jsx)(n.A,{lg:22,md:22,sm:22,xs:48,children:(0,w.jsxs)(x,{children:[(0,w.jsx)("h6",{children:g(t)}),(0,w.jsx)(p,{children:g(d)}),"right"===b?(0,w.jsx)(j,{children:"object"===typeof u&&u.map(((e,i)=>(0,w.jsx)(c.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("about"),children:g(e.title)},i)))}):(0,w.jsx)(m,{children:(0,w.jsx)(r.A,{justify:"space-between",children:"object"===typeof l&&l.map(((e,i)=>(0,w.jsxs)(n.A,{span:11,children:[(0,w.jsx)(o.A,{src:e.icon,width:"60px",height:"60px"}),(0,w.jsx)(f,{children:g(e.title)}),(0,w.jsx)(y,{children:g(e.content)})]},i)))})})]})})})})})}))}}]);
+//# sourceMappingURL=566.28fe90bc.chunk.js.map
